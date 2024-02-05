01ff92b85a516b9d8d9b491e4f922f553c7e721d
kitouo <3544457932@qq.com>
2024-02-05 21:58:59 +0800

    create server

--- server.go
+++ server.go
@@ -13,23 +13,43 @@ type Server struct {
 
 // 创建一个server接口
 func NewServer (ip string, port int) *Server{
-	return &server {
+	return &Server {
 		Ip : ip,
-		Port : port
+		Port : port,
 	}
+}
+
+func (server *Server) Handler(conn net.Conn) {
+	// 当前连接的业务
+	fmt.Println("连接建立成功")
+
 }
 
 // 启动服务器接口
 func (server *Server) Start() {
 	// socket listen
-	net.Listen("tcp", fmt.Sprintf("%s:%d", server.Ip, server.Port))
-	// accpet
-
-	// handle
-
-	// close listen socket
-}
-
-func main() {
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", server.Ip, server.Port))
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+		
+	}
 	
+	// close listen socket
+	defer listener.Close()
+
+	for {
+		// accept
+		conn, err := listener.Accept()
+
+		if err != nil {
+			fmt.Println("listener accept err:", err)
+			continue
+		}
+
+		// handler
+		go server.Handler(conn)
+
+	}
+
 }
